0157ae0d18a3e7f43501b23c520983a966f5fd7b
Marcos Sales <m4rcos.sales@gmail.com>
2024-07-21 01:49:19 -0300

    feat: add imagens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: DefaultLoginLayoutComponent
+  }
+];
