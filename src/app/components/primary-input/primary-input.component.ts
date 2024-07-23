33d13e94a10407880f9a67d796d1102199adee4e
Marcos Sales <m4rcos.sales@gmail.com>
2024-07-23 02:07:48 -0300

    feat: alteracoes no login

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 type InputTypes = "text" | "email" | "password"
 
@@ -7,11 +7,55 @@ type InputTypes = "text" | "email" | "password"
   selector: 'app-primary-input',
   standalone: true,
   imports: [ReactiveFormsModule],
+
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(()=> PrimaryInputComponent),
+      multi: true
+    }
+  ],
+
   templateUrl: './primary-input.component.html',
   styleUrl: './primary-input.component.scss'
 })
-export class PrimaryInputComponent {
+export class PrimaryInputComponent implements ControlValueAccessor{
   @Input() type: InputTypes = "text";
-  @Input() formName: InputTypes = "text";
-  @Input() placeholder: InputTypes = "text";
+  @Input() placeholder: string = "";
+  @Input() label: string = "";
+  @Input() inputName: string = "";
+
+  value: string = ''
+  onChange: any = () => {}
+  onTouched: any = () => {}
+
+
+  writeValue(obj: any): void {
+    this.value = obj
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+   this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onInput(event: Event){
+    const value = (event.target as HTMLInputElement).value
+    this.onChange(value)
+  }
+
+  // writeValue(value: any): void{
+  //   this.value = value
+  // }
+
+  // registerOnChange(fn: any){
+  //   this.onChange = fn
+  // }
+
+  // setDisabledState(isDisabled: boolean):void{}
 }
+
